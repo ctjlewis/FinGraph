5b632ae0ee90ce47f6c6874fb579ceada4161214
Christian Lewis <1657236+ctjlewis@users.noreply.github.com>
2021-06-09 14:19:16 -0500

    chore: build scripts

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='FinGraph',  
-     version='0.0.2',
+     version='0.0.3',
     #  scripts=['dokr'] ,
      author="C. Lewis",
      author_email="ctj.lewis@icloud.com",
